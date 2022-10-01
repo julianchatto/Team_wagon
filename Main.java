8933ecd1d9503791911aaac953ddeae74e84bfe0
Anya <anyarayyy@dyn196026.cc.lehigh.edu>
2022-10-01 13:56:20 -0400

    main method

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class mainPage
+{
+    private String invites;
+    private String leaderBoard;
+    private String login; // link to login page
+    private String signUp; // link to sign up page if not already registered
+    private int scanIn; // scan a  qr code when going to the frats
+
+    
+}
+    
+    

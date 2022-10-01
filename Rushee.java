62dc3dd8c9c2347c7d2d7041e2b4cbedd1d586ea
julianchatto <juc226@lehigh.edu>
2022-10-01 13:40:37 -0400

    Rushee object added

---
+++ Rushee.java
@@ -0,0 +1,19 @@
+public class Rushee {
+    private String username;
+    private String password;
+    private int classYear;
+    private String name;
+    private Frat topBid;
+    private String description;
+
+    public Rushee(String username, String password, int classYear, String name, Frat topBid, String description) {
+        this.username = username;
+        this.password = password;
+        this.classYear = classYear;
+        this.name = name;
+        this.topBid = topBid;
+        this.description = description;
+    }
+    
+
+}

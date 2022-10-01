b2521adc76fd7ed7b6232a34745066c4bf015f40
julianchatto <juc226@lehigh.edu>
2022-10-01 16:25:15 -0400

    Stuff

--- Rushee.java
+++ Rushee.java
@@ -4,13 +4,11 @@ public class Rushee extends User{
     private int classYear;
     
     private Frat topBid;
-    private ArrayList<String> friends; 
 
     public Rushee(String name, String username, String password, int classYear, Frat topBid, String description) {
         super(username, password, description,name);
         this.classYear = classYear;
         this.topBid = topBid;
-        this.friends = friends;
     }
 
     //Getters
@@ -25,10 +23,6 @@ public class Rushee extends User{
         return topBid; 
     }
 
-
-    public ArrayList<String> getfriends(){
-        return friends;
-    }
     
     public void setClassYear(int c) {
         classYear = c;
@@ -37,8 +31,6 @@ public class Rushee extends User{
     public void settopBid(Frat tb){
         topBid = tb;
     }
-    public void setFriends(ArrayList<String> f){
-        friends = f;
-    }
+
 
 }
